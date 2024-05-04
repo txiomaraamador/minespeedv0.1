81300f221108b037b65c0a5a81646cb069573957
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2024-05-04 15:47:56 -0600

    validacion de areas

--- resources/views/AreasCreate.blade.php
+++ resources/views/AreasCreate.blade.php
@@ -14,18 +14,27 @@
                 
                 <div class="mb-3">
                     <label for="name" class="form-label">Nombre</label>
-                    <input type="text" class="form-control" id="name" name="name">
+                    <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" id="name" name="name" required>
+                    @error('name')
+                    <div class="invalid-feedback" style="color: red;">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="topographic_information" class="form-label">Información Topográfica</label>
-                    <input type="text" class="form-control" id="topographic_information" name="topographic_information">
+                    <input type="text" class="form-control{{ $errors->has('topographic_information') ? ' is-invalid' : '' }}" id="topographic_information" name="topographic_information" required>
+                    @error('topographic_information')
+                    <div class="invalid-feedback" style="color: red;">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
 
                 <div class="row">
                     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                         <a href="{{ route('areas.index') }}" class="btn btn-secondary">Cancelar</a>
                         <button type="submit" class="btn btn-primary mr-2" style="background-color: #ee194f; border-color: #ee194f; color: #fff;">Guardar</button>
-                       
                     </div>
                 </div>
                 
--- resources/views/AreasEdit.blade.php
+++ resources/views/AreasEdit.blade.php
@@ -16,13 +16,23 @@
                     <div class="col-md-6">
                         <div class="mb-3">
                             <label for="name" class="form-label">Nombre</label>
-                            <input type="text" class="form-control" id="name" name="name" value="{{ $area->name }}">
+                            <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" id="name" name="name" value="{{ $area->name }}">
+                            @error('name')
+                                <div class="invalid-feedback" style="color: red;">
+                                 {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="mb-3">
                             <label for="topographic_information" class="form-label">Información Topográfica</label>
-                            <input type="text" class="form-control" id="topographic_information" name="topographic_information" value="{{ $area->topographic_information }}">
+                            <input type="text" class="form-control{{ $errors->has('topographic_information') ? ' is-invalid' : '' }}" id="topographic_information" name="topographic_information" value="{{ $area->topographic_information }}">
+                            @error('topographic_information')
+                            <div class="invalid-feedback" style="color: red;">
+                                {{ $message }}
+                            </div>
+                            @enderror
                         </div>
                     </div>
                 </div>
