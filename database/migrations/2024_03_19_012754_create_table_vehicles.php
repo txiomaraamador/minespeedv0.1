3978b143b20b5a9f5957171a1b22c40b7f592ff0
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2024-05-20 15:34:05 -0600

    migracion correccion

--- database/migrations/2024_03_19_012754_create_table_vehicles.php
+++ database/migrations/2024_03_19_012754_create_table_vehicles.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_vehicles');
+        Schema::dropIfExists('vehicles');
     }
 };
--- database/migrations/2024_03_19_012853_create_table_vehicle_area.php
+++ database/migrations/2024_03_19_012853_create_table_vehicle_area.php
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_vehicle_area');
+        Schema::dropIfExists('vehicle_area');
     }
 };
