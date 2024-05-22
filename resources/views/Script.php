f95c3aa16359dd022589cce3bc3f3615994d7f17
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2024-05-22 09:49:23 -0600

    x

---
+++ resources/views/Script.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$dbconn = pg_connect("host=127.0.0.1 dbname=minespeedv01 user=postgres password=root");
+pg_query($dbconn, "LISTEN report_insert");
+
+while (true) {
+    $notification = pg_get_notify($dbconn, PGSQL_ASSOC);
+    if ($notification !== false) {
+        // Aquí puedes ejecutar la lógica correspondiente, como llamar a la ruta 'correos'
+        // Por ejemplo:
+        file_get_contents('http://127.0.0.1:8000/correos');
+    }
+    // Esperar un segundo antes de verificar nuevas notificaciones
+    sleep(10);
+}
+?>
